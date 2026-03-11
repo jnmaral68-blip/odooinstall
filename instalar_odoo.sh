0791d65f2ac8b18b945e34cbaf3795eed6220433
jnmaral68-blip <jnmaral68@gmail.com>
2026-03-11 14:38:43 +0100

    Cambio ajuste permisos

--- instalar_odoo.sh
+++ instalar_odoo.sh
@@ -14,18 +14,13 @@ cd /home/jnmar
 sudo mkdir -p /etc/odoo /var/log/odoo "$DIR_BASE"
 sudo apt update && sudo apt install -y git postgresql
 
-# 2. Ajustar permisos (Crucial para que Odoo funcione)
-echo "--- Ajustando permisos ---"
-sudo chown -R odoo:odoo /opt/odoo
-sudo chown -R odoo:odoo /var/log/odoo
-
-# 3. Clonar Odoo Base (OCB)
+# 2. Clonar Odoo Base (OCB)
 if [ ! -d "/opt/odoo/odoo" ]; then
     echo "--- Clonando OCB ---"
     git clone --depth 1 --branch $BRANCH https://github.com/SOLDIGES/OCB.git /opt/odoo/odoo
 fi
 
-# 4. Clonar repositorios desde la lista
+# 3. Clonar repositorios desde la lista
 if [ -f "$LISTA_REPOS" ]; then
     while IFS= read -r repo || [ -n "$repo" ]; do
         # Ignorar líneas vacías o comentarios
@@ -38,6 +33,12 @@ else
     echo "Error: No se encontró el archivo $LISTA_REPOS"
     exit 1
 fi
+
+# 4. Ajustar permisos (Crucial para que Odoo funcione)
+echo "--- Ajustando permisos ---"
+sudo chown -R odoo:odoo /opt/odoo
+sudo chown -R odoo:odoo /var/log/odoo
+sudo chmod -R 775 /opt/odoo/odoo
 
 echo "¡Proceso finalizado!"
 Usa el código con precaución.
